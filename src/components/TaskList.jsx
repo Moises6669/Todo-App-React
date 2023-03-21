10ddef49e0226f2b276d941b7af9d13bbf8b1cd7
Nobody6669 <moisesalex132@gmail.com>
2023-03-21 06:06:58 -0600

    ADD: remove tasks from the list

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,10 +1,10 @@
 import { TaskCard } from "./TaskCard";
 
-export const TaskList = ({ tasks }) => {
+export const TaskList = ({ tasks, deleteTask }) => {
   return (
     <>
       {tasks.map((task) => (
-        <TaskCard key={task.id} task={task} />
+        <TaskCard key={task.id} task={task} deleteTask={deleteTask} />
       ))}
     </>
   );
